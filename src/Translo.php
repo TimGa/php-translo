ceca4487618842d0ec5c6dca040b57c4874daf71
TimGa <tgavryutenko@mail.ru>
2022-09-23 14:18:25 +0300

    init: create php-translo

---
+++ src/Translo.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Translo;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
+
+class Translo
+{
+    private Client $client;
+    private const HOST = 'translo.p.rapidapi.com';
+    private const BASE_URL = 'https://' . self::HOST . '/api/v3/';
+    private string $apiKey;
+
+    public function __construct(string $apiKey)
+    {
+        $this->apiKey = $apiKey;
+        $this->client = new Client([
+            'base_uri' => self::BASE_URL,
+        ]);
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function translate(string $text, string $fromLang, string $toLang): array
+    {
+        $response = $this->client->request('POST', 'translate', [
+            'headers' => [
+                'content-type' => 'application/x-www-form-urlencoded',
+                'X-RapidAPI-Key' => $this->apiKey,
+                'X-RapidAPI-Host' => self::HOST
+            ],
+            'form_params' => [
+                'from' => $fromLang,
+                'to' => $toLang,
+                'text' => $text
+            ]
+        ]);
+        return $this->responseToArray($response);
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function batchTranslate(array $body): array
+    {
+        $response = $this->client->request('POST', 'batch_translate', [
+            'headers' => [
+                'X-RapidAPI-Key' => $this->apiKey,
+                'X-RapidAPI-Host' => self::HOST
+            ],
+            'json' => $body
+        ]);
+        return $this->responseToArray($response);
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function detect(string $text): array
+    {
+        $response = $this->client->request('GET', 'detect', [
+            'headers' => [
+                'X-RapidAPI-Key' => $this->apiKey,
+                'X-RapidAPI-Host' => self::HOST
+            ],
+            'query' => [
+                'text' => $text
+            ]
+        ]);
+        return $this->responseToArray($response);
+    }
+
+    private function responseToArray(ResponseInterface $response): array
+    {
+        return json_decode($response->getBody()->getContents(), true);
+    }
+}
